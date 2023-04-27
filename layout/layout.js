fdcec94af1be586f04f65c9bbc871f1b80664669
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2023-04-28 02:29:44 +0330

    update

--- layout/layout.js
+++ layout/layout.js
@@ -5,21 +5,23 @@ import * as Linking from 'expo-linking';
 
 const Stack = createNativeStackNavigator();
 const Layout = () => {
-  const config = {
-    screens: {
-      Home: '/:restaurantLink',
-    },
-  };
-
   const linking = {
-    prefixes: ['https://menu.tarefilfiley.me'],
-    config,
+    prefixes: [],
+    config: {
+      screens: {
+        Home: ':restaurantLink',
+      },
+    },
   };
 
   return (
     <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
       <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ route }) => ({ restaurantLink: `${route.params.restaurantLink}` })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
